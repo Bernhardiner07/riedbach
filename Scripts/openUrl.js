822a4fafc07bce8fef0e862c5747aec07bd85907
Nils Wittwer <nils.wittwer@bsfh-lernende.ch>
2022-07-05 13:54:16 +0200

    Finished Project Prompt

--- Scripts/openUrl.js
+++ Scripts/openUrl.js
@@ -2,10 +2,16 @@ const buttonMaps = document.getElementById('openMaps')
 
 const buttonSBB = document.getElementById('openSBB')
 
+const clickingLogo = document.getElementById('logo')
+
 buttonMaps.addEventListener('click', () => {
     window.open('https://goo.gl/maps/dvSd84NqHxP1Z7vC6')
 })
 
 buttonSBB.addEventListener('click', () => {
     window.open('https://www.sbb.ch/de/kaufen/pages/fahrplan/fahrplan.xhtml')
+})
+
+clickingLogo.addEventListener('click', () => {
+    open('../index.html')
 })
